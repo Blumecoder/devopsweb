f4c986adbbd9ba44b57d0c705ed3c4a5efbba104
Blumecoder <116629712+Blumecoder@users.noreply.github.com>
2022-10-25 23:04:39 +0530

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace devopsweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
